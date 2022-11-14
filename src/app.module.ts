482436328abee9b37f3dcb105031ea3650bc6ff8
scuti-loctd <loctd@scuti.asia>
2022-11-14 17:19:47 +0700

    up[date

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CatModule } from './cat/cat.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, CatModule],
+  imports: [
+    AuthModule,
+    CatModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      database: 'test',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true,
+      username: 'root',
+      password: '123456',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from './account.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { LocalStrategy } from './strategy/local.strategy';
 @Module({
-  providers: [AuthService],
+  imports: [
+    TypeOrmModule.forFeature([Account]),
+    PassportModule,
+    JwtModule.register({
+      secret: '1993',
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
   controllers: [AuthController],
+  exports: [AuthService, TypeOrmModule, JwtModule],
 })
 export class AuthModule {}
